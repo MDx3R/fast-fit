cacbc87d7c22053e543cfacf778bf2e189c3428f
MDx3R <nationmdxr@gmail.com>
2025-11-02 16:24:16 +0300

    fix: use factory to create dish

--- src/fastfit/menu/application/usecases/command/create_dish_use_case.py
+++ src/fastfit/menu/application/usecases/command/create_dish_use_case.py
@@ -1,4 +1,4 @@
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from fastfit.menu.application.dtos.commands.create_dish_command import CreateDishCommand
 from fastfit.menu.application.interfaces.repositories.dish_repository import (
@@ -7,31 +7,32 @@ from fastfit.menu.application.interfaces.repositories.dish_repository import (
 from fastfit.menu.application.interfaces.usecases.command.create_dish_use_case import (
     ICreateDishUseCase,
 )
-from fastfit.menu.domain.entities.dish import Dish
-from fastfit.menu.domain.value_objects.dish_filters import DishFilters
-from fastfit.menu.domain.value_objects.dish_name import DishName
-from fastfit.menu.domain.value_objects.ingredients import Ingredients
-from fastfit.menu.domain.value_objects.money import Money
-from fastfit.menu.domain.value_objects.nutritional_info import NutritionalInfo
+from fastfit.menu.domain.interfaces.dish_factory import DishFactoryDTO, IDishFactory
 
 
 class CreateDishUseCase(ICreateDishUseCase):
-    def __init__(self, dish_repository: IDishRepository) -> None:
+    def __init__(
+        self, dish_factory: IDishFactory, dish_repository: IDishRepository
+    ) -> None:
+        self.dish_factory = dish_factory
         self.dish_repository = dish_repository
 
     async def execute(self, command: CreateDishCommand) -> UUID:
-        dish = Dish.create(
-            dish_id=uuid4(),
-            name=DishName.create(command.name),
-            description=command.description,
-            price=Money.create(command.price, command.currency),
-            nutritional_info=NutritionalInfo.create(
-                command.calories, command.proteins, command.fats, command.carbohydrates
-            ),
-            ingredients=Ingredients.create(command.ingredients),
-            filters=DishFilters.create(command.filters),
-            category_id=command.category_id,
-            restaurant_id=command.restaurant_id,
+        dish = self.dish_factory.create(
+            DishFactoryDTO(
+                name=command.name,
+                description=command.description,
+                currency=command.currency,
+                price=command.price,
+                calories=command.calories,
+                proteins=command.proteins,
+                fats=command.fats,
+                carbohydrates=command.carbohydrates,
+                ingredients=command.ingredients,
+                filters=command.filters,
+                category_id=command.category_id,
+                restaurant_id=command.restaurant_id,
+            )
         )
         await self.dish_repository.add(dish)
         return dish.dish_id
